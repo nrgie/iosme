a995a848d07e2567312166ab2f23d8a4633e397c
Tibor Sulyok <tiborsulyok@Tibors-iMac.lan>
2018-03-17 13:29:22 -0700

    fixes

--- saveme/GuardsMapViewController.swift
+++ saveme/GuardsMapViewController.swift
@@ -11,53 +11,45 @@ import UIKit
 import GoogleMaps
 
 class GuardsMapViewController: UIViewController {
-
-    let mapback : MapBackView = MapBackView(frame: CGRect.zero)
     
     var locationManager = LocationManager.sharedInstance
+
+    @IBOutlet weak var map: GMSMapView!
+
+    @IBAction func backbtn(_ sender: Any) {
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "base", bundle: nil)
+        let baseController = mainStoryboard.instantiateViewController(withIdentifier: "BaseViewController") as! BaseViewController
+        UIApplication.shared.delegate?.window??.rootViewController = baseController
+    }
     
-    let didFindMyLocation = false
-    
+    @IBOutlet weak var backlogo: UIImageView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        
-        
-        
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true
-        view = mapView
+        let camera = GMSCameraPosition.camera(withLatitude: 33.86, longitude: 21.20, zoom: 6.0)
+        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        map.isMyLocationEnabled = true
+        map.camera = camera
         
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 33.86, longitude: 21.20)
         marker.title = "Budapest"
         marker.snippet = ""
-        marker.map = mapView
-
-        self.view.addSubview(mapback)
-        self.view.bringSubview(toFront: mapback)
+        marker.map = map
         
-        mapback.isUserInteractionEnabled = true
+        backlogo.isUserInteractionEnabled = true
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(userDidTapLabel(tapGestureRecognizer:)))
-        mapback.addGestureRecognizer(tapGesture)
+        backlogo.addGestureRecognizer(tapGesture)
         
 
-        
-        //mapback.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
-        
-        let mapInsets = UIEdgeInsets(top: 80.0, left: 0.0, bottom: 45.0, right: 0.0)
-        mapView.padding = mapInsets
-        
-        // GOOGLE MAPS SDK: COMPASS
-        mapView.settings.compassButton = true
-        
-        // GOOGLE MAPS SDK: USER'S LOCATION
-        mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
-        
+        //let mapIns = UIEdgeInsets(top: 80.0, left: 0.0, bottom: 45.0, right: 0.0)
+        //map.padding = mapIns
+
+        map.settings.compassButton = true
+        map.isMyLocationEnabled = true
+        map.settings.myLocationButton = true
         
         locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
             if error != nil {
